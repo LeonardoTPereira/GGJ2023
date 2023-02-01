931c09df6e622160ca6871124f2c8f40cb2012de
angelobguido <angelobguido@gmail.com>
2023-02-01 04:50:11 -0300

    ✨Feature: Implement EnemyMoveTwoWay

--- Assets/Scripts/Enemies/EnemyMoveTwoWay.cs
+++ Assets/Scripts/Enemies/EnemyMoveTwoWay.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class EnemyMoveTwoWay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private Transform pointA;
+    [SerializeField] private Transform pointB;
+
+    private Transform currentPoint;
+    
+    private Rigidbody2D enemyRb;
+
+    private void Awake()
     {
-        
+        enemyRb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
+        currentPoint = pointA;
+        enemyRb.velocity = transform.forward * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        if (CheckFlip())
+        {
+            Flip();
+        }
+
+        if (CheckPointReached())
+        {
+            ChangePoint();
+        }
+    }
+
+    private bool CheckFlip()
+    {
+        var currentTransform = transform;
+        return (currentPoint.position - currentTransform.position).x * (currentTransform.forward).x < 0;
+    } 
+    
+    private void Flip()
+    {
+        var currentScale = transform.localScale;
+
+        transform.localScale = new Vector3(-currentScale.x, currentScale.y, currentScale.z);
+
+    }
+
+    private bool CheckPointReached()
+    {
+        return Vector3.Distance(transform.position, currentPoint.position) < 0.1f;
+    }
+
+    private void ChangePoint()
+    {
+        if (currentPoint == pointA)
+            currentPoint = pointB;
         
+        else if (currentPoint == pointB)
+            currentPoint = pointA;
     }
 }

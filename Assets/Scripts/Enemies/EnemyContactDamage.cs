ceb3ddd3fe7436d895a8d32788595d0870694b00
angelobguido <angelobguido@gmail.com>
2023-02-01 04:17:02 -0300

    ✨Feature: Implement EnemyMoveLeft

--- Assets/Scripts/Enemies/EnemyContactDamage.cs
+++ Assets/Scripts/Enemies/EnemyContactDamage.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 
 public class EnemyContactDamage : MonoBehaviour
 {
+    [SerializeField] private int contactDamage = 1;
+    
     private void OnTriggerEnter(Collider other)
     {
         //Hit player
--- Assets/Scripts/Enemies/EnemyMoveLeft.cs
+++ Assets/Scripts/Enemies/EnemyMoveLeft.cs
@@ -1,18 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMoveLeft : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed = 10f;
+    
+    private Rigidbody2D enemyRb;
+
+    private void Awake()
     {
-        
+        enemyRb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        enemyRb.velocity = transform.forward * speed;
     }
+
 }

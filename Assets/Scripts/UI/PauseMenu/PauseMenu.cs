f3b0defe5c385135e119658d88cca6682783a315
Tyago Teoi <tyago.teoi@usp.br>
2023-02-06 12:40:50 -0300

    :bug:Fix: pause menu not pausing the game

--- Assets/Scripts/UI/PauseMenu/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu/PauseMenu.cs
@@ -10,15 +10,14 @@ public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private VisualElement _root;
     private VisualElement _pauseMenu;
-
     private bool _isPauseMenuActive;
 
     private void Start()
     {
 
         VisualElement _root = GetComponent<UIDocument>().rootVisualElement;
-
         _pauseMenu = _root.Q<VisualElement>("PauseMenu");
+
         Button startButton = _pauseMenu.Q<Button>("main-menu");
         Button exitButton = _pauseMenu.Q<Button>("exit");
 
@@ -34,19 +33,13 @@ public class PauseMenu : MonoBehaviour
     {
         if (context.performed)
         {
-            _isPauseMenuActive = !_isPauseMenuActive;
-            //UIUtils.Display(_pauseMenu, _isPauseMenuActive);
-            tes(_pauseMenu, _isPauseMenuActive);
-        }
-    }
+            Time.timeScale = 0;
+            if (_isPauseMenuActive)
+                Time.timeScale = 1;
 
-    public void tes(VisualElement panel, bool finalState)
-    {
-        if (panel == null)
-        {
-            Debug.Log("aaaaaaaa");
-            return;
+            _isPauseMenuActive = !_isPauseMenuActive;
+            UIUtils.Display(_pauseMenu, _isPauseMenuActive);
+
         }
-        panel.style.display = finalState ? DisplayStyle.Flex : DisplayStyle.None;
     }
 }

3dffb89a2a1b1876d6280aa63e22c00096875059
Tyago Teoi <tyago.teoi@usp.br>
2023-02-20 22:24:16 -0300

    :sparkles:Feature: Add Audio, AudioManager and Sound

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using Assets.Scripts.Audio;
+
+// How to use:
+// Inside some script of an object you desire to play a sound listed on the AudioManager
+// you can simply type: 
+// ***************** 'AudioManager.Instance.Play("Name of the sound");' ******************
+// passing the name of the sound to play it.
+
+public class AudioManager : Audios
+{
+    // 'instance' references to itself
+    public static AudioManager Instance;
+
+    // Awake is called before the Start method
+    protected override void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            GameObject.Destroy(this);
+
+        base.Awake();
+    }
+}

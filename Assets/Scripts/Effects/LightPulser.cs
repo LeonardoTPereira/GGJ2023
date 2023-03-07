9996f36e9dd9774e3a3571e299dda3c2a4398d56
Amanda <amanda.lrib@gmail.com>
2023-03-06 21:22:27 -0300

    🔧Modify offset and light intensity

--- Assets/Scripts/Effects/LightPulser.cs
+++ Assets/Scripts/Effects/LightPulser.cs
@@ -7,7 +7,7 @@ public class LightPulser : MonoBehaviour
 {
     [SerializeField] private float duration;
     private Light2D light;
-    private float offset = 1f;
+    private float offset = 2f;
 
     void Start()
     {
@@ -17,7 +17,7 @@ public class LightPulser : MonoBehaviour
     void FixedUpdate()
     {
         float phi = (Time.time / duration) * Mathf.PI;
-        float amplitude = Mathf.Cos(phi) * 0.5f + offset;
+        float amplitude = (Mathf.Cos(phi) * 0.5f + offset)/2;
         light.intensity = amplitude;
     }
 }

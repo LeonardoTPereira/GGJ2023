8d08ae440535b52495f789ad2e7b7f9b584b79b7
Tyago Teoi <tyago.teoi@usp.br>
2023-03-09 21:01:39 -0300

    :sparkles:Feature: Pause menu UI functionalities

---
+++ Assets/Scripts/UI/PlayMenu/PauseMenu/PauseMenuPresenter.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+using UI.Utils;
+using UnityEngine.InputSystem;
+using System;
+using static UnityEditor.Recorder.OutputPath;
+
+namespace UI.Presenters
+{
+    public class PauseMenuPresenter : MonoBehaviour
+    {
+        public Action ResumeAction { set => _resumeButton.clicked += value; }
+
+        private VisualElement _frontPage;
+        private VisualElement _settingsPage;
+
+        private Button _resumeButton;
+        private Button _settingsButton;
+        private Button _mainMenuButton;
+        private Button _exitButton;
+
+        private Button _backButton;
+        private Slider _mainVolumeSlider;
+        private Slider _musicVolumeSlider;
+        private Slider _sfxVolumeSlider;
+
+        public PauseMenuPresenter(VisualElement root)
+        {
+            _frontPage = root.Q<VisualElement>("front-menu");
+            _settingsPage = root.Q<VisualElement>("settings-menu");
+
+            SetoutFrontPageButtons();
+            SetoutSettingsPageButtons();
+            SetoutSettingsVolumeSliders();
+
+            UIUtils.Display(_frontPage, false);
+        }
+
+        private void SetoutFrontPageButtons()
+        {
+            _resumeButton = _frontPage.Q<Button>("resume-button");
+            _settingsButton = _frontPage.Q<Button>("settings-button");
+            _mainMenuButton = _frontPage.Q<Button>("main-menu-button");
+            _exitButton = _frontPage.Q<Button>("exit-game-button");
+
+            _resumeButton.clicked += () => ClosePauseMenuSettings(true);
+            _settingsButton.clicked += () => ClosePauseMenuSettings(false);
+            _mainMenuButton.clicked += MainMenuPopUp;
+            _exitButton.clicked += ExitPopUp;
+        }
+
+        private void SetoutSettingsPageButtons()
+        {
+            _backButton = _settingsPage.Q<Button>("back-button");
+            _mainVolumeSlider = _settingsPage.Q<Slider>("main-volume-slider");
+            _musicVolumeSlider = _settingsPage.Q<Slider>("music-volume-slider");
+            _sfxVolumeSlider = _settingsPage.Q<Slider>("sfx-volume-slider");
+
+            _backButton.clicked += () => ClosePauseMenuSettings(false);
+        }
+
+        private void SetoutSettingsVolumeSliders()
+        {
+            SetoutSlider(_settingsPage, _mainVolumeSlider, "MainVolume", "main-volume-slider");
+            SetoutSlider(_settingsPage, _musicVolumeSlider, "MusicVolume", "music-volume-slider");
+            SetoutSlider(_settingsPage, _sfxVolumeSlider, "SfxVolume", "sfx-volume-slider");
+        }
+
+        private void SetoutSlider(VisualElement root, Slider slider, string mixerVolumeName, string sliderName)
+        {
+            slider = root.Q<Slider>(sliderName);
+
+            float mixerValue;
+            AudioManager.Instance.GetMainMixer().GetFloat(mixerVolumeName, out mixerValue);
+            slider.value = AudioManager.Instance.ConvertVolumeToSliderValue(mixerValue);
+
+            slider.RegisterValueChangedCallback((value) => AudioManager.Instance.SetVolume(mixerVolumeName, value.newValue, true));
+        }
+
+        private void ClosePauseMenuSettings(bool enable)
+        {
+            _frontPage.Display(enable);
+            _settingsPage.Display(!enable);
+        }
+
+        // Should ideally create a popup and ask the player if really wants to go back to main menu
+        private void MainMenuPopUp()
+        {
+#if UNITY_EDITOR
+            Debug.Log("BACK MAIN MENU PRESSED");
+            return;
+#endif
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+
+        // Should ideally create a popup and ask the player if really wants to exit the game
+        private void ExitPopUp()
+        {
+            Debug.Log("EXIT BUTTON PRESSED");
+            Application.Quit();
+        }
+    }
+}
--- Assets/Scripts/UI/PlayMenu/PlayMenuPresenter.cs
+++ Assets/Scripts/UI/PlayMenu/PlayMenuPresenter.cs
@@ -3,26 +3,59 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UI.Utils;
+using UnityEngine.InputSystem;
 
-public class PlayMenuPresenter : MonoBehaviour
+namespace UI.Presenters
 {
-    private VisualElement _lifeBar;
-    private VisualElement _pauseMenu;
-    //private VisualElement _settingsMenu;
-    //private VisualElement _gameoverMenu;
-    private VisualElement _winMenu;
-
-    void OnEnable()
+    public class PlayMenuPresenter : MonoBehaviour
     {
-        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
-        _lifeBar = root.Q<VisualElement>("life-bar-container");
-        _pauseMenu = root.Q<VisualElement>("pause-menu-container");
-        _winMenu = root.Q<VisualElement>("win-menu-container");
-    }
+        private VisualElement _lifeBar;
+        private VisualElement _pauseMenu;
+        //private VisualElement _settingsMenu;
+        //private VisualElement _gameoverMenu;
+        private VisualElement _winMenu;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private bool _isPaused;
+
+        void Start()
+        {
+            _isPaused = false;
+
+            VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+            _lifeBar = root.Q<VisualElement>("life-bar-container");
+            _pauseMenu = root.Q<VisualElement>("pause-menu-container");
+            _winMenu = root.Q<VisualElement>("win-menu-container");
+        }
+
+        private void SetPauseMenuEvents()
+        {
+            PauseMenuPresenter pauseMenuPresenter = new PauseMenuPresenter(_pauseMenu);
+            pauseMenuPresenter.ResumeAction = () => TogglePauseMenu(false);
+            pauseMenuPresenter.ResumeAction = () => PauseOrUnpause();
+        }
+
+        private void TogglePauseMenu(bool enable)
+        {
+            _lifeBar.Display(!enable);
+            _pauseMenu.Display(enable);
+        }
+
+        private void PauseOrUnpause()
+        {
+            Time.timeScale = 0;
+            if (_isPaused)
+                Time.timeScale = 1;
+
+            _isPaused = !_isPaused;
+        }
+        public void PressPauseButton(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                PauseOrUnpause();
+                TogglePauseMenu(_isPaused);
+            }
+        }
     }
 }

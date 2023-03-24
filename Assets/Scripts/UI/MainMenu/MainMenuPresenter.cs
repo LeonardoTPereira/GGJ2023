829af4ad6ede83359c7a23aa004a2fafda24669c
Tyago Teoi <tyago.teoi@usp.br>
2023-03-24 20:09:02 -0300

    :bug:Fix: MainMenuPresenter.cs using Recorder.OutputPath

--- Assets/Scripts/UI/MainMenu/MainMenuPresenter.cs
+++ Assets/Scripts/UI/MainMenu/MainMenuPresenter.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
-using static UnityEditor.Recorder.OutputPath;
 
 public class MainMenuPresenter
 {

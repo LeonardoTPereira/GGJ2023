5cb79a4b694ad111ffe7381d91183c4cc25c3e37
Amanda <amanda.lrib@gmail.com>
2023-05-31 22:45:28 -0300

    🐛Fix: Death particle not playing

--- Assets/Scripts/Entity/Enemy/Health.cs
+++ Assets/Scripts/Entity/Enemy/Health.cs
@@ -6,6 +6,9 @@ namespace Enemy
 {
     public class Health : Entity.Health
     {
+        [SerializeField] private ParticleSystem explosionDeathParticle;
+        [SerializeField] private ParticleSystem damageBloodParticle;
+        [SerializeField] private Collider2D damageCollider;
         [SerializeField] private DamageEffect damageEffect;
         protected override void WhenInitializeHealth()
         {
@@ -15,12 +18,16 @@ namespace Enemy
         protected override void WhenKill()
         {
             //Trigger death animation
+            damageCollider.enabled = false;
+            var explosionDeathObject = Instantiate(explosionDeathParticle, transform.position, Quaternion.identity);
+            explosionDeathObject.Play();
             Destroy(gameObject, timeToDestroyObject);
         }
 
         protected override void WhenTakeDamage(int damage)
         {
             //Trigger take damage animation
+            damageBloodParticle.Play();
             damageEffect.BlinkDamage();
         }
 
